567943c4f51047a3bf7711ad2cd38c66e68777e3
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-02-04 14:39:16 -0700

    moved site to handlebars and added fortune feature

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,46 +1,33 @@
 const express = require('express')
+const fortune = require("./lib/fortune")
+const expressHandlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' })
+
 const app = express()
-
-const handlebars = require('express-handlebars')
-handlebars.create({ defaultLayout: 'main' })
-
-app.engine('handlebars', handlebars.engine)
+app.engine('handlebars', expressHandlebars.engine)
 app.set('view engine', 'handlebars')
 app.set('port', process.env.PORT || 3030)
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
+// set up routes
+app.get('/', (req, res) => res.render('home'))
+app.get('/about', (req, res) => res.render('about', { fortune: fortune.randomFortune() }))
 
-app.get('/about/contact', (req, res) => {
-    res.type('text/plain')
-    res.send('Contact Meadowlark Travel')
-})
-
-app.get('/about/directions', (req, res) => {
-    res.type('text/plain')
-    res.send('Directions to Meadowlark Travel')
-})
-
-app.get('/about*', (req, res) => {
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
-})
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 — Not Found')
+    res.render('404')
 })
 app.use((err, req, res, next) => {
     console.log(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 — Server Error')
+    res.render('500')
 })
 
+// start server
 app.listen(app.get('port'), () => console.log(
-    `Express started on http://localhost:${app.get('port')}; ` +
-    `pres Ctrl-C to terminate.`
+    `Express started on http://localhost:${app.get('port')} ` +
+    `press Ctrl-C to terminate.`
 ))
+
+
