275c7fa529090e36f55d59b49b1135e3a241a812
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-02-08 10:56:07 -0700

    removed fortune feature from about page

--- site/lib/handlers.js
+++ site/lib/handlers.js
@@ -1,7 +1,5 @@
-const fortune = require('./fortune');
-
 exports.home = (_req, res) => res.render('home')
-exports.about = (_req, res) => res.render('about', { fortune: fortune.randomFortune() });
+exports.about = (_req, res) => res.render('about');
 exports.notFound = (_req, res) => res.status(404).render('404', { layout: 'main' })
 exports.serverError = (err, _req, res, _next) => res.status(500).render('500', { error: err })
 
